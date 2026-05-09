7c0987e36b6449b618a20e7d7d1ba3ea053d05a8
chirag gupta <chirag.97735248@gmail.com>
2026-05-10 04:28:37 +0530

    ui changes

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let isPlaying = false;
+let queue = 0;
+
+const Songs = [
+  {
+    song: "./audio-assets/HBT.mp3",
+    title: "Hitori Bocchi Tokyo",
+    cover: "./audio-assets/audio-cover-art/hitori-bocchi-tokyo.png",
+  },
+
+  {
+    song: "./audio-assets/If I could be a constellation.mp3",
+    title: "If I Could Be a Constellation",
+    cover: "./audio-assets/audio-cover-art/IcouldBC.webp",
+  },
+
+  {
+    song: "./audio-assets/Rockn' Roll, Morning Light Falls on You.mp3",
+    title: "Rockn' Roll, Morning Light Falls on You",
+    cover: "./audio-assets/audio-cover-art/IcouldBC.webp",
+  },
+];
+
+let CurrentSong = new Audio(Songs[queue].song);
 
 const PpButton = document.getElementById("pause-play");
-
-const HBT = new Audio("./audio-assets/HBT.mp3");
+const ForwardButton = document.getElementById("forward");
+const BackwardButton = document.getElementById("backward");
 
 const CoverArt = document.getElementById("sample");
-const HBT_ART = "./assets/hitori-bocchi-tokyo.png";
-const BaseArt = "./assets/bocchi-the-rock.jpg";
+const SongTitle = document.getElementById("SongName");
+const VolumeSlider = document.getElementById("volume-slider");
 
 const PlaySvg = `<svg width="40" height="40" viewBox="0 0 24 24" fill="none">
   <rect x="6" y="4" width="4" height="16" rx="1" fill="currentColor"/>
@@ -17,17 +40,71 @@ const PauseSvg = `<svg width="40" height="40" viewBox="0 0 24 24" fill="none">
   <path d="M8 5V19L19 12L8 5Z" fill="currentColor"/>
 </svg>`;
 
-PpButton.addEventListener("click", function () {
+const UpdateUI = () => {
+  CoverArt.src = Songs[queue].cover;
+
+  SongTitle.textContent = Songs[queue].title;
+};
+
+const ChangeSong = () => {
+  CurrentSong.pause();
+
+  CurrentSong = new Audio(Songs[queue].song);
+
+  CurrentSong.volume = VolumeSlider.value / 100;
+  UpdateUI();
+
+  if (isPlaying) {
+    CurrentSong.play();
+  }
+};
+
+UpdateUI();
+
+PpButton.addEventListener("click", () => {
   isPlaying = !isPlaying;
 
   if (isPlaying) {
     PpButton.innerHTML = PlaySvg;
-    CoverArt.src = HBT_ART;
-    HBT.play();
+
+    CurrentSong.play();
   } else {
     PpButton.innerHTML = PauseSvg;
-    CoverArt.src = BaseArt;
-    HBT.currentTime = 0;
-    HBT.pause();
+
+    CurrentSong.pause();
   }
+});
+
+ForwardButton.addEventListener("click", () => {
+  queue++;
+
+  if (queue >= Songs.length) {
+    queue = 0;
+  }
+
+  ChangeSong();
+});
+
+BackwardButton.addEventListener("click", () => {
+  queue--;
+
+  if (queue < 0) {
+    queue = Songs.length - 1;
+  }
+
+  ChangeSong();
+});
+
+CurrentSong.addEventListener("ended", () => {
+  queue++;
+
+  if (queue >= Songs.length) {
+    queue = 0;
+  }
+
+  ChangeSong();
+});
+
+VolumeSlider.addEventListener("input", () => {
+  CurrentSong.volume = VolumeSlider.value / 100;
 });
